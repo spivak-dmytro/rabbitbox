d16aa9c61a45f68c2d619ec6f6b59c41bf002000
Dmytro Spivak <spivak.dmytro@coaxsoft.com>
2023-01-04 10:27:36 +0200

    Add action validator

---
+++ helpers/messageActionValidator.js
@@ -0,0 +1,9 @@
+/**
+ * If the message is an object, and it has an action property that matches the actionName parameter, then return true.
+ * @param msg - The message that was sent to the bot.
+ * @param actionName - The name of the action to validate against.
+ */
+const messageActionValidator = (msg, actionName) =>
+  typeof msg === 'object' && msg !== null && msg.action === actionName;
+
+export default messageActionValidator;

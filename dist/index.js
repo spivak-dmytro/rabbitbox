7d033366bd3aba10c519ff0a6b4a2b561e0f80b2
Dmytro Spivak <spivak.dmytro@coaxsoft.com>
2023-01-15 15:18:00 +0200

    fix: exports

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,21 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
-var _saga = _interopRequireDefault(require("./saga"));
-var _action = _interopRequireDefault(require("./action"));
+exports.helpers = exports.Saga = exports.Action = void 0;
+var Saga = _interopRequireWildcard(require("./saga"));
+exports.Saga = Saga;
+var Action = _interopRequireWildcard(require("./action"));
+exports.Action = Action;
 var _msgActionValidator = _interopRequireDefault(require("./helpers/msgActionValidator"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 // helpers
-var _default = {
-  Saga: _saga["default"],
-  Action: _action["default"],
-  helpers: {
-    msgActionValidator: _msgActionValidator["default"]
-  }
+
+var helpers = {
+  msgActionValidator: _msgActionValidator["default"]
 };
-exports["default"] = _default;
+exports.helpers = helpers;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-export Saga from './saga';
-export Action from "./action";
+import * as Saga from './saga';
+import * as Action from "./action";
 // helpers
 import msgActionValidator from "./helpers/msgActionValidator";
 
 export const helpers = {
   msgActionValidator,
 };
+
+export {
+  Saga,
+  Action,
+}

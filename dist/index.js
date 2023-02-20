41ca02c4021cc0fdd02932316e1a7e6fefc7107e
Dmytro Spivak <spivak.dmytro@coaxsoft.com>
2023-02-20 23:54:15 +0200

    fix: exports

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,26 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+Object.defineProperty(exports, "Action", {
+  enumerable: true,
+  get: function get() {
+    return _action["default"];
+  }
+});
+Object.defineProperty(exports, "Saga", {
+  enumerable: true,
+  get: function get() {
+    return _saga["default"];
+  }
+});
+exports.helpers = void 0;
 var _saga = _interopRequireDefault(require("./saga"));
 var _action = _interopRequireDefault(require("./action"));
 var _msgActionValidator = _interopRequireDefault(require("./helpers/msgActionValidator"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 // helpers
-var _default = {
-  Saga: _saga["default"],
-  Action: _action["default"],
-  helpers: {
-    msgActionValidator: _msgActionValidator["default"]
-  }
+
+var helpers = {
+  msgActionValidator: _msgActionValidator["default"]
 };
-exports["default"] = _default;
+exports.helpers = helpers;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import Saga from './saga';
-import Action from "./action";
+export { default as Saga } from './saga';
+export { default as Action } from "./action";
 // helpers
 import msgActionValidator from "./helpers/msgActionValidator";
 
-export default {
-  Saga,
-  Action,
-  helpers: {
-    msgActionValidator,
-  },
+export const helpers = {
+  msgActionValidator,
 };

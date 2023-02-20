039ec57ecd008b13b9e0529e3670c2a9829e0544
Dmytro Spivak <spivak.dmytro@coaxsoft.com>
2023-02-21 00:08:13 +0200

    fix: exports

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,11 @@
 "use strict";
 
-var _msgActionValidator = _interopRequireDefault(require("./helpers/msgActionValidator"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var Saga = require('./saga');
 var Action = require('./action');
 // helpers
-
+var msgActionValidator = require('./helpers/msgActionValidator');
 module.exports = {
   Saga: Saga,
   Action: Action,
-  msgActionValidator: _msgActionValidator["default"]
+  msgActionValidator: msgActionValidator
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const Saga = require('./saga');
 const Action = require('./action');
 // helpers
-import msgActionValidator from "./helpers/msgActionValidator";
+const msgActionValidator = require('./helpers/msgActionValidator');
 
 module.exports = {
   Saga,
